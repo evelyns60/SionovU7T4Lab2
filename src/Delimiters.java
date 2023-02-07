5c7ea783048e956bd932b08d2495602e3d30ec91
student <evelyns60@nycstudents.net>
2023-02-07 10:59:57 -0500

    2/7/23 class push

---
+++ src/Delimiters.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Delimiters
+{
+    /** The open and close delimiters **/
+    private String openDel;
+    private String closeDel;
+
+    /** Constructs a Delimiters object where open is the open delimiter and close is the
+     *  close delimiter.
+     *  Precondition: open and close are non-empty strings.
+     */
+    public Delimiters(String open, String close)
+    {
+        openDel = open;
+        closeDel = close;
+    }
+
+    /** Returns an ArrayList of delimiters from the array tokens, as described in part (a). */
+    public ArrayList<String> getDelimitersList(String[] tokens){
+        ArrayList<String> delimiterList = new ArrayList<>();
+        for (String token : tokens) {
+            if (token.equals(openDel) || token.equals(closeDel)) {
+                delimiterList.add(token);
+            }
+        }
+        return delimiterList;
+    }
+
+
+    /** Returns true if the delimiters are balanced and false otherwise, as described in part (b).
+     *  Precondition: delimiters contains only valid open and close delimiters.
+     */
+    public boolean isBalanced(ArrayList<String> delimiters){
+        int numOpen = 0;
+        int numClosed = 0;
+        for (String delimiter : delimiters){
+            if (delimiter.equals(openDel)) {
+                numOpen ++;
+            } else {
+                numClosed ++;
+            }
+
+            if (numClosed > numOpen) {
+                return false;
+            }
+        }
+        if (numOpen != numClosed) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
